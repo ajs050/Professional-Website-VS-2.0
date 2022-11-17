1b0023ea57528e21e4f98ff7529e6cc08aae38b7
Andrew <ajsantos050@gmail.com>
2022-11-17 18:17:33 -0500

    Added shapes

--- apps.js
+++ apps.js
@@ -1,8 +1,23 @@
 // template_yjj2txf
 // service_b939bzk
 // zwmj78c78DIVSZt8W
-
+let IsModelOpen = false
 let contrastToggle = false
+const scaleFactor = 1 / 20
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll('.shape')
+  const x = event.clientX * scaleFactor
+  const y = event.clientY * scaleFactor
+  
+  for(let i = 0; i< shapes.length; i++) {
+    const isOdd = i % 2 !== 0
+    const boolInt = isOdd ? -1 : 1
+    shapes[i].style.transform = 'translate(' + x * boolInt+ 'px,' + y * boolInt + 'px)'
+  }
+}
+
+
 function toggleContrast() {
     contrastToggle = !contrastToggle
     if(contrastToggle) {
@@ -36,7 +51,7 @@ function contact(event) {
       })
 }
 
-let IsModelOpen = false
+
 function toggleModel() {
     if (IsModelOpen) {
         IsModelOpen = false
